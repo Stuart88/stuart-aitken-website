4eb66b497b0e9505f2047f0213655e9eb071abcc
Stuart Aitken <s.aitken88@gmail.com>
2021-09-18 15:48:35 +0700

    Add functions API and basic cosmos db

---
+++ stuart-aitken-website/API/Function1.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace API
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            CosmosDbClient db = new CosmosDbClient();
+
+            var items = await db.QueryItemsAsync(1);
+
+            return new OkObjectResult(items);
+        }
+    }
+}
+
---
+++ stuart-aitken-website/Shared/DbModels/PortfolioProject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DbModels
+{
+    public class PortfolioProject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ProjectDate { get; set; }
+        public int ProjectDurationWeeks { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string Tech { get; set; }
+        public string Urls { get; set; }
+        public int? Views { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int Status { get; set; }
+    }
+}
