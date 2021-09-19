674a4e2d4250a5ac6e765d748d13473ce950c9e7
Stuart Aitken <s.aitken88@gmail.com>
2021-09-19 17:44:35 +0700

    styling changes

--- stuart-aitken-website/Shared/DbModels/PortfolioProject.cs
+++ stuart-aitken-website/Shared/DbModels/PortfolioProject.cs
@@ -9,13 +9,13 @@ namespace SharedProject.DbModels
     public class PortfolioProject
     {
         public int ProjectId { get; set; }
-        public string Name { get; set; }
-        public DateTime ProjectDate { get; set; }
+        public string Name { get; set; } = "";
+        public DateTime ProjectDate { get; set; } = DateTime.Now;
         public double ProjectDurationWeeks { get; set; }
-        public string Type { get; set; }
-        public string Description { get; set; }
-        public string Tech { get; set; }
-        public string Urls { get; set; }
+        public string Type { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Tech { get; set; } = "";
+        public string Urls { get; set; } = "";
         public int? Views { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime ModifiedDate { get; set; }
